cc9325da4b8c89eb8dd46123a55bcc4a6779b868
VladStoyanoff <vladstoyanoff@gmail.com>
2022-08-15 15:19:13 +0100

    camera movement

---
+++ Tankio/Tankio/Assets/Scripts/Cameras/CameraController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+using Mirror;
+
+public class CameraController : NetworkBehaviour
+{
+    [SerializeField] Transform playerCameraTransform;
+    [SerializeField] float speed;
+    [SerializeField] float screenBorderThickness;
+    [SerializeField] Vector2 screenXLimits;
+    [SerializeField] Vector2 screenZLimits;
+
+    Vector2 previousInput;
+
+    Controls controls;
+
+    public override void OnStartAuthority()
+    {
+        playerCameraTransform.gameObject.SetActive(true);
+
+        controls = new Controls();
+
+        controls.Player.MoveCamera.performed += SetPreviousInput;
+        controls.Player.MoveCamera.canceled += SetPreviousInput;
+
+        controls.Enable();
+    }
+
+    void Update()
+    {
+        if (!hasAuthority || !Application.isFocused) return;
+
+        UpdateCameraPosition();
+    }
+
+    void UpdateCameraPosition()
+    {
+        Vector3 pos = playerCameraTransform.position;
+
+        if(previousInput == Vector2.zero)
+        {
+            var cursorMovement = Vector3.zero;
+            var cursorPosition = Mouse.current.position.ReadValue();
+
+            if(cursorPosition.y >= Screen.height - screenBorderThickness)
+            {
+                cursorMovement.z += 1;
+            }
+            else if(cursorPosition.y <= screenBorderThickness)
+            {
+                cursorMovement.z -= 1;
+            }
+            if (cursorPosition.y >= Screen.width - screenBorderThickness)
+            {
+                cursorMovement.x += 1;
+            }
+            else if (cursorPosition.y <= screenBorderThickness)
+            {
+                cursorMovement.x -= 1;
+            }
+
+            pos += cursorMovement.normalized * speed * Time.deltaTime;
+        }
+        else
+        {
+            pos += new Vector3(previousInput.x, 0f, previousInput.y) * speed * Time.deltaTime;
+        }
+
+        pos.x = Mathf.Clamp(pos.x, screenXLimits.x, screenXLimits.y);
+        pos.z = Mathf.Clamp(pos.z, screenZLimits.x, screenZLimits.y);
+
+        playerCameraTransform.position = pos;
+    }
+
+    void SetPreviousInput(InputAction.CallbackContext ctx)
+    {
+        previousInput = ctx.ReadValue<Vector2>();
+    }
+}
